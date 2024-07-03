52ad1e22aa7488a3f099dffeabca4b0913a3062c
LarisAires0307 <aires.larissa2005@gmail.com>
2024-07-02 22:49:13 -0300

    primeiro commit

---
+++ imagens/questions.js
@@ -0,0 +1,110 @@
+export default [
+    {
+        question:"Pergunta 1:",
+        answers: [
+            {  option: "X", correct: true  },
+            {  option: "Y", correct: false  },
+            {  option: "Z", correct: false  },
+
+        ],
+    },
+    {
+        question:"Pergunta 2:",
+        answers: [
+            {  option: "X", correct: false  },
+            {  option: "Y", correct: true  },
+            {  option: "Z", correct: false  },
+
+        ],
+    },
+    {
+        question:"Pergunta 3:",
+        answers: [
+            {  option: "X", correct: false  },
+            {  option: "Y", correct: true  },
+            {  option: "Z", correct: false  },
+
+        ],
+    },
+    {
+        question:"Pergunta 4:",
+        answers: [
+            {  option: "X", correct: false  },
+            {  option: "Y", correct: false  },
+            {  option: "Z", correct: true  },
+
+        ],
+    },
+    {
+        question:"Pergunta 5:",
+        answers: [
+            {  option: "X", correct: true  },
+            {  option: "Y", correct: false  },
+            {  option: "Z", correct: false  },
+
+        ],
+    },
+    {
+        question:"Pergunta 6:",
+        answers: [
+            {  option: "X", correct: false  },
+            {  option: "Y", correct: true  },
+            {  option: "Z", correct: false  },
+
+        ],
+    },
+    {
+        question:"Pergunta 7:",
+        answers: [
+            {  option: "X", correct: true  },
+            {  option: "Y", correct: false  },
+            {  option: "Z", correct: false  },
+
+        ],
+    },
+    {
+        question:"Pergunta 8:",
+        answers: [
+            {  option: "X", correct: false  },
+            {  option: "Y", correct: false  },
+            {  option: "Z", correct: true  },
+
+        ],
+    },
+    {
+        question:"Pergunta 9:",
+        answers: [
+            {  option: "X", correct: true  },
+            {  option: "Y", correct: false  },
+            {  option: "Z", correct: false  },
+
+        ],
+    },
+    {
+        question:"Pergunta 10:",
+        answers: [
+            {  option: "X", correct: false  },
+            {  option: "Y", correct: true  },
+            {  option: "Z", correct: false  },
+
+        ],
+    },
+    {
+        question:"Pergunta 11:",
+        answers: [
+            {  option: "X", correct: false  },
+            {  option: "Y", correct: false  },
+            {  option: "Z", correct: true  },
+
+        ],
+    },
+    {
+        question:"Pergunta 12:",
+        answers: [
+            {  option: "X", correct: true  },
+            {  option: "Y", correct: false  },
+            {  option: "Z", correct: false  },
+
+        ],
+    },
+];
